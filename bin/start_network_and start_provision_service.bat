834f8453a7d939a6bfca2b3988c9c0ec3b7fae8f
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-12-06 15:56:20 +0100

    adjust bootstrap node ip

--- bin/start_network_and start_provision_service.bat
+++ bin/start_network_and start_provision_service.bat
@@ -2,5 +2,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -b 172.30.160.1:9011 -p 9013 uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.monitoring.provision.MonitoringDataProvisionService','MDPSPass') interactive
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -b 169.254.118.232:9011 -p 9013 uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.monitoring.provision.MonitoringDataProvisionService','MDPSPass') interactive
 pause
