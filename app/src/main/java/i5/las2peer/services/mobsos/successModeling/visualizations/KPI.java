9a7497822da1c1a175899803f2349cc4ff47a38a
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-05-03 11:56:52 +0200

    remove unused import

--- app/src/main/java/i5/las2peer/services/mobsos/successModeling/visualizations/KPI.java
+++ app/src/main/java/i5/las2peer/services/mobsos/successModeling/visualizations/KPI.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
